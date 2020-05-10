cb5b4649a8b7885912c297555755f1e6feb864c8
khushboo2803 <khushboogrover.mca19.du@gmail.com>
2020-05-10 14:11:59 +0530

    updated

--- Screens/admin.js
+++ Screens/admin.js
@@ -1,8 +1,34 @@
 import React from 'react';
-import { Button, View, Text, TextInput, StyleSheet, TouchableOpacity, Alert , BackHandler} from 'react-native';
+import { Button, View, Text, TextInput, StyleSheet, TouchableOpacity, Alert , BackHandler, Image, Dimensions, KeyboardAvoidingView, ImageBackground} from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 
-function checkCredentials(user, password)
+//             onPress={()=>{
+//               if(checkCredentials(value, pass))
+//               {
+//                 navigation.navigate('list');
+//               }
+//               else{
+//                 Alert.alert("Wrong Credentials");
+//               }
+//             }}>
+
+const device_width= Dimensions.get('window').width;
+const device_height=Dimensions.get('window').height;
+export default class admin extends React.Component
+{
+  constructor(props){
+    super(props);
+    this.state={
+      showPass:true,
+      press:false,
+      user:'',
+      pass:'',
+      text:''
+    };
+    this.showPass=this.showPass.bind(this);
+  }
+
+  checkCredentials(user, password)
 {
   if(user=='Khushboo' && password=='280397')
   {
@@ -12,112 +38,133 @@ function checkCredentials(user, password)
     return false;
   }
 }
+  changeUser(name)
+  {
+    this.state.text=name;
+  }
+  changePass(pass)
+  {
+    this.state.pass=pass;
+  }
 
-
-export default function Admin(props){
-  const [value, onChangeText] = React.useState('');
-  const [pass, onChangePass] = React.useState('');
-  const {navigation}=props;
-
- 
-    return (
-      <View style={{backgroundColor:'cornflowerblue'}}>
-        <View
-          style={{
-            marginTop: '15%',
-            marginBottom: '40%',
-            marginLeft: '5%',
-            borderRadius: 50,
-            borderWidth: 2,
-            backgroundColor: 'white',
-            width: '90%',
-            height: '81%',
-            marginRight: '20%',
-          }}
+  showPass(){
+    this.state.press===false ? this.setState({showPass: false, press: true}): this.setState({showPass: true, press: false});
+  }
+  render(){
+    return(
+      <ImageBackground source={require('../assets/admin.jpg')}
+      style={{
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'cover'
+      }}>
+  
+        <KeyboardAvoidingView behavior="padding"
+        style={{
+          flex: 1,
+          alignItems:'center'
+        }}
         >
-          <View style={{flexDirection:'row'}}>
-            <Text
-            style={
-              {
-                marginTop: '40%',
-                marginLeft: '15%',
-                fontSize:20,
-                fontWeight:'bold', 
-              }}>
-                Name :
-            </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'mintcream',
+              borderWidth: .5,
+              borderColor: '#000',
+              height: 40,
+              borderRadius: 5 ,
+              margin: 10,
+              marginTop: '60%',
+              width: '90%'
+            }}
+          >
+          <Image source={require('../assets/user.png')} 
+          style={{
+            height:30,
+            width:30
+          }}/>
             <TextInput
-              style={{
-                marginTop: '40%',
-                marginLeft: '5%',
-                borderRadius: 5,
-                borderWidth: 1,
-                borderBottomColor: 'black',
-                width: '50%'
-              }}
-              onChangeText={text=>{onChangeText(text)}}
-              placeholder="Admin name here.."
-              value={value}
-            />
+            style={{
+              backgroundColor: 'rgba(255,255,255,0.4)',
+              flex:1,
+              color: 'black',
+              fontFamily: 'monospace'
+            }}
+            placeholder="Enter your name here.. "
+            placeholderTextColor="darkgray"
+            underlineColorAndroid="transparent"
+            onChangeText={text=>{this.changeUser(text)}}
+            defaultValue={this.state.text}
+          />
           </View>
 
-          <View style={{flexDirection:'row'}}>
-            <Text
-            style={
-              {
-                marginTop: '10%',
-                marginLeft: '4%',
-                fontSize:20,
-                fontWeight:'bold', 
-              }}>
-                Password :
-            </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'mintcream',
+              borderWidth: .5,
+              borderColor: '#000',
+              height: 40,
+              borderRadius: 5 ,
+              margin: 10,
+              marginTop: '9%',
+              width: '90%'
+            }}
+          >
+          <Image source={require('../assets/pass.png')} 
+          style={{
+            height:30,
+            width:30
+          }}/>
             <TextInput
-              style={{
-                marginTop: '10%',
-                marginLeft: '5%',
-                borderRadius: 5,
-                borderWidth: 1,
-                borderBottomColor: 'black',
-                width: '50%'
-              }}
-              onChangeText={text=>{onChangePass(text)}}
-              placeholder="password name here.."
-              secureTextEntry={true}
-              value={pass}
-            />
+            style={{
+              backgroundColor: 'rgba(255,255,255,0.4)',
+              flex:1,
+              color: 'black',
+              fontFamily: 'monospace'
+            }}
+            placeholder="Enter password here.. "
+            placeholderTextColor="darkgray"
+            underlineColorAndroid="transparent"
+            onChangeText={text=>{this.changePass(text)}}
+            defaultValue={this.state.pass}
+            secureTextEntry={true}
+          />
           </View>
 
           <View>
-            <TouchableOpacity style={
-              {
-                borderWidth:2,
-                borderRadius:5,
-                marginLeft:'30%',
-                marginRight: '30%',
-                marginTop: '20%',
-                backgroundColor:'dodgerblue'
-              }
-            }
-            onPress={()=>{
-              if(checkCredentials(value, pass))
-              {
-                navigation.navigate('list');
-              }
-              else{
-                Alert.alert("Wrong Credentials");
-              }
-            }}>
-                <Text style={{fontWeight:'bold', fontSize: 30}}>  Submit</Text>
+            <TouchableOpacity onPress={()=>{
+              if(this.checkCredentials(this.state.text, this.state.pass))
+                             {
+                               this.props.navigation.navigate('list');
+                             }
+                             else{
+                               Alert.alert("Wrong Credentials");
+                             }
+                           }
+            }>
+              <Image
+                source={require('../assets/submit.jpg')}
+                style={{
+                  height:110, 
+                  width:110,
+                  borderRadius:65,
+                  marginTop:'30%',
+                  borderWidth:5,
+                  borderColor:'olivedrab'
+                }}
+              />
             </TouchableOpacity>
           </View>
-
-          <View 
-          style={{marginTop: '71%'}}>
-            <Text style={{color:'red'}}> Disclaimer * This functionality is only for admins. Do not try to violate the privacy.</Text>
-          </View>
-
-        </View>
-      </View>
+        </KeyboardAvoidingView>
+  
+      </ImageBackground>
     );
+  }
+  
 }
--- Screens/trials.js
+++ Screens/trials.js
@@ -1,5 +1,5 @@
-import React,{useState, useRef} from 'react';
-import { Animated, View, StyleSheet, Alert, PanResponder, Text, Image, TouchableOpacity, ActivityIndicator, BackHandler } from "react-native";
+import React,{useState, useRef, useEffect} from 'react';
+import { Animated, View, StyleSheet, Alert, PanResponder, Text, Image, TouchableOpacity, ActivityIndicator, BackHandler, ImageBackground, Dimensions } from "react-native";
 import { TextInput } from 'react-native-gesture-handler';
 import fun from '../functions/fun.js';
 import db from '../functions/dbhandler.js';
@@ -7,111 +7,112 @@ import styles from './style.js';
 var address=[];
 var blank=[];
 
-
 async function checkifExist(text, props)
-{
-    if(text<999999999 || text>9999999999)
-    {
-        Alert.alert('Invalid Number');
-    }
-    const response = await db.isExist(text);
-    if(response)
-    {    
-        const { navigation } = props;
-        address=await db.getAddress(text)
-        const cord = [];
-        address.forEach(async (data) => {
-            data = JSON.parse(data);
-            cord.push({ latitude: data.lat, longitude: data.lon }); 
-        });
-        navigation.navigate('location', { address, cord });
-    }
-    else{
-        const { navigation } = props;
-            Alert.alert("no such user", "This number hasn't registered yet",
-            [{text:'ok'}]);
-            navigation.navigate('home');
-            
-        }
-}
+ {
+     if(text<999999999 || text>9999999999)
+     {
+         Alert.alert('Invalid Number');
+         return;
+     }
+     const response = await db.isExist(text);
+     if(response)
+     {    
+         const { navigation } = props;
+         address=await db.getAddress(text)
+         const cord = [];
+         address.forEach(async (data) => {
+             data = JSON.parse(data);
+             cord.push({ latitude: data.lat, longitude: data.lon }); 
+         });
+         navigation.navigate('location', { address, cord });
+     }
+     else{
+         const { navigation } = props;
+             Alert.alert("no such user", "This number hasn't registered yet",
+             [{text:'ok'}]);
+             return ;
+         }
+ }
 
-export default function showTrials(props)
+ function backAction(){
+
+    
+    Alert.alert("Hold on!", "Are you sure you want to exit?", [
+      {
+        text: "Cancel",
+        onPress: () => null,
+        style: "cancel"
+      },
+      { text: "YES", onPress: () => BackHandler.exitApp() }
+    ]);
+    return true;
+  };
+
+export default function main(props)
 {
-    const {navigation}=props;
-    const pan = useRef(new Animated.ValueXY()).current;
-    const panResponder = useRef(
-        PanResponder.create({
-        onMoveShouldSetPanResponder: () => true,
-        onPanResponderMove: Animated.event([
-            null,
-            { dx: pan.x, dy: pan.y }
-        ]),
-        onPanResponderRelease: () => {
-            Animated.spring(pan, { toValue: { x: 0, y: 0 } }).start();
-        }
-        })
-    ).current;
+    useEffect(() => {
+        BackHandler.addEventListener("hardwareBackPress", backAction);
+        return () =>
+          BackHandler.removeEventListener("hardwareBackPress", backAction);
+      }, []);
+
     const [text, setText] = useState('');
     return(
-        <View style={styles.container}>
-      <Animated.View
-        style={{
-          transform: [{ translateX: pan.x }, { translateY: pan.y }]
-        }}
-        {...panResponder.panHandlers}
-      >
-        <View>
-          <Image source={require('../assets/image2.png')}
-          style={{height:190, width:190, marginLeft:'25%', marginTop:'40%'}}></Image>
-        </View>
-        
-        </Animated.View>  
-        <TextInput
-                     style={{
-                        height: 22,
-                        borderBottomWidth:3, 
-                        width:'80%', 
-                        borderBottomColor:'blue',
-                        alignContent:'center',
-                        alignItems: 'center',
-                        marginTop:'10%',
-                        marginLeft:'10%',
-                        marginRight:'10%'
-                     }}
-                     placeholder="             Type your phone number here!"
-                     onChangeText={text => setText(text)}
-                     defaultValue={text}
-                     placeholderTextColor="black"
-                     keyboardType="numeric"
-                  />
-
-          <ActivityIndicator size="large" color="#0000ff" />  
-        
-        <View>
-            <TouchableOpacity 
-                style={{marginTop:'7%', 
-                        marginLeft:'40%',
-                        borderWidth:2,
-                        width:'25%',
-                        height:30,
-                        borderRadius:4,
-                        backgroundColor:'cornflowerblue'
-
+        <ImageBackground
+            source={require('../assets/search.jpg')}
+            style={{
+                height:Dimensions.get('window').height,
+                width:Dimensions.get('window').width,
+            }}
+        >
+            <View style={{
+                
+                flexDirection:'row',
+                borderWidth:3,
+                height:60,
+                marginLeft:'5%',
+                marginRight:'2%',
+                marginTop:(Dimensions.get('window').height)*0.50,
+                borderRadius:15,
+                backgroundColor:'white'
+            }}>
+                <Image
+                    source={require('../assets/search-icon.png')}
+                    style={{
+                        height:48,
+                        width:48,
+                        marginLeft:'2%'
                     }}
-                onPress={
-                         ()=>{
-                             checkifExist(text, props)}  
-                         }>
-                             <Text style={{
-                                 fontSize:18
-                             }}>  Proceed</Text>
-                         </TouchableOpacity>
-        </View>
-        
-     
-
-      
-    </View>
-    
+                />
+            <TextInput
+                      style={{
+                         fontSize:18,
+                         backgroundColor:'white',
+                         fontFamily:'monospace',
+                         width:'80%'
+                      }}
+                      placeholder="Type phone number here!"
+                      onChangeText={text => setText(text)}
+                      defaultValue={text}
+                      placeholderTextColor="black"
+                      keyboardType="numeric"
+                   />
+            </View>
+            <TouchableOpacity onPress={
+                          ()=>{
+                              checkifExist(text, props)}  
+                          }>
+                <Image source={require('../assets/search-button.jpg')}
+                style={{
+                    height:'29%', width: '52%',
+                    borderRadius:120,
+                    backgroundColor:'transparent',
+                    marginLeft:(Dimensions.get('window').width)*0.25,
+                    marginTop:'6%',
+                    borderColor:'olivedrab',
+                    borderWidth:5
+                }} />
+            </TouchableOpacity>
+        </ImageBackground>
     );
 }
